355ad886f54f1c25f7aa474fcf3516b2aa73f774
Gabriel de Lisle <gldl@kth.se>
2019-02-05 22:19:05 +0100

    clean

--- display.py
+++ display.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-with open("color.txt", 'r') as file: 
-	f = lambda x: (int(x)+2)**0.1
-	matrix = [list( map(f, u.split(" ")[:-1]) ) for u in file]
+
+with open("color"+str(i)+".txt", 'r') as file: 
+	f = lambda x: int(x)**0.7 #more contrast for lower values
+	matrix = np.array([list( map(f, u.split(" ")[:-1]) ) for u in file])
 	
-plt.matshow(matrix, cmap = plt.cm.magma)
-plt.savefig("figure.pdf")
-plt.show()
+plt.imsave("figure"+str(i)+".png", matrix, cmap = plt.cm.magma)
--- parallel.c
+++ parallel.c
@@ -1,60 +1,56 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <math.h>
-#define M 256
-#define N 256
 
-#define B 4
-#define Nmax 256
-#define Ncolor 256
-#define P 16
-//#define DEBUG 1
+#define M 2048 // Rows
+#define N 2048 // Columns
+#define B 2 // Threshold 
+#define x0 0.25 // X axis coordinate in the center
+#define y0 0.0 // Y axis coordinate in the center 
+#define zoom 1.0/256 // width (or height) of the picture
+
+#define Nmax 256 // Number of colors
+#define P 128// Number of processes -1
 
 
-int mandelbrot(float d1, float d2){
-	int count = 1;
+int mandelbrot(float d1, float d2) {
+	int count = 0;
 	float z1 = 0; float z2 = 0;
 	float zz1 = 0; float zz2 = 0;
-	while ((z1*z1 + z2*z2 < B) & (count < Nmax)){
+
+	// the loops keep running until its norm is higher than the threshold or the maximum number of iterations is reached
+	while ((z1*z1 + z2*z2 < B*B) & (count < Nmax)){
+		// z = z^2 + d 
 		zz1 = z1; zz2 = z2;
 		z1 = zz1*zz1 - zz2*zz2 + d1;
 		z2 = 2*zz1*zz2 + d2;
 		count++;
 	}
-	return count % Ncolor;
+	return count;
 }
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int rank, size, tag, rc, i,j,t;
 	tag = 0;
+	
+	// Init MPI
 	MPI_Status status;
 
 	rc = MPI_Init(&argc, &argv);
 	rc = MPI_Comm_size(MPI_COMM_WORLD, &size);
 	rc = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	float x0 = 0; float y0 = 0;
-	float zoom = 2;
-	
 
 	if (rank == 0) {
+		// root process collect computed pixels and write it in the file
 		FILE *fp;
-		/* your computations */
 		fp = fopen("color.txt","w");
 		for (t = 0; t < P; t++) {
 
 			unsigned char color[M*N/P];
 			rc = MPI_Recv(color, M*N/P, MPI_UNSIGNED_CHAR, t+1, tag, MPI_COMM_WORLD, &status);
 			
-			// printf("received ");
-			// for (i = 0; i < M*N/P; ++i) {
-			// 	printf("%hhu ", color[i]);
-			// }
-			// printf("\n");
-
-
 			for (j = 0; j < N/P; j++) {
 				for (i = 0; i < M; i++)
 				   fprintf(fp, "%hhu ", color[i+j*M]);
@@ -65,27 +61,23 @@ int main(int argc, char *argv[])
 		
 	}
 	else{
+		// each of the P other process compute a part of the picture
 		unsigned char color[M*N/P];
 		int k;
 		for (j = 0; j < N/P; j++) {
 			for (i = 0; i < M; i++) {
 				k = (rank-1)*N/P+j;
-			   color[i+j*M] = mandelbrot(x0 + zoom * (2.*i/(M-1) -1), y0 + zoom * (2.*k/(N-1) -1));
-
-				//printf("point: %d, %d, %f, %f \n", i,k,x0 + zoom * (2.*i/(M-1) -1), y0 + zoom * (2.*k/(N-1) -1));
-
+				// coordinates of each pixel
+				float d1 = x0 + zoom * (2.*i/(M-1) -1);
+				float d2 = y0 + zoom * (2.*k/(N-1) -1);
+            color[i+j*M] = mandelbrot(d1, d2);
 			}
 		}
 		rc = MPI_Send(color, M*N/P, MPI_UNSIGNED_CHAR, 0, tag, MPI_COMM_WORLD);
-		
-		// printf("sent ");
-		// for (i = 0; i < M*N/P; ++i) {
-		// 	printf("%hhu ", color[i]);
-		// }
-		// printf("\n");
+
 	}
+	rc = MPI_Finalize();
 
 		
 	return 0;
 }
-	
